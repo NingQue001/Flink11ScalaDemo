4a054e2d193f92dacd6151e876d5807118aabd28
NingQue001 <296480474@qq.com>
2020-07-27 22:24:53 +0800

    feat: Table Batch Table Demo

---
+++ src/main/java/com/anven/jtableAndSql/JavaBatchWordCount.java
@@ -0,0 +1,53 @@
+package com.anven.jtableAndSql;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple1;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
+import org.apache.flink.types.Row;
+
+public class JavaBatchWordCount {
+    public static void main(String[] args) throws Exception {
+        // batch mode 使用的是旧版的planner
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        BatchTableEnvironment tEnv = BatchTableEnvironment.create(env);
+
+        String path = JavaBatchWordCount.class
+                .getClassLoader()
+                .getResource("words.txt")
+                .getPath();
+
+        /**
+         * How to get a table?
+         */
+
+        // 1. Table Descriptor
+//        tEnv.connect(new FileSystem().path(path))
+////                .withFormat(new OldCsv().field("word", Types.STRING).lineDelimiter("\n"))
+//                .withFormat(new Csv().fieldDelimiter('\n'))
+//                .withSchema(new Schema().field("word", Types.STRING))
+//                .createTemporaryTable("words");
+
+        // 2. User defined table source
+//        TableSource csvSource = new CsvTableSource(path, new String[]{"word"}, new TypeInformation[]{Types.STRING});
+//        tEnv.registerTableSource("words", csvSource);
+
+        // 3. Register a Dataset
+        DataSource<Tuple1<String>> dataSet = env.readCsvFile(path)
+//                .fieldDelimiter("\n")
+//                .lineDelimiter(",")
+                .includeFields(true)
+                .ignoreInvalidLines()
+                .types(String.class);
+//        dataSet.print();
+        tEnv.registerDataSet("words", dataSet, "word");
+
+        Table table = tEnv.scan("words")
+                .groupBy("word")
+                .select("word, count(1) as count");
+
+        tEnv.toDataSet(table, Row.class).print();
+
+    }
+}
--- src/main/scala/com/anven/tableAndSql/TableDemo.scala
+++ src/main/scala/com/anven/tableAndSql/TableDemo.scala
@@ -1,17 +1,66 @@
-package com.anven.tableAndSql
-
-import org.apache.flink.streaming.api.datastream.DataStream
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
-
-
-object TableDemo {
-  def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment()
-
-    val text: DataStream[String] = env.readTextFile("file:///usr/local/tmp/list.txt")
-
-    text.print()
-
-    env.execute()
-  }
-}
+//package com.anven.tableAndSql
+//
+//import java.util.Properties
+//
+//import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+//import org.apache.flink.streaming.util.serialization.SimpleStringSchema
+//import org.apache.flink.table.api.{EnvironmentSettings, Table}
+//// implicit
+//import org.apache.flink.streaming.api.scala._
+//import org.apache.flink.table.api._
+//import org.apache.flink.api.scala._
+//
+//object TableDemo {
+//  def main(args: Array[String]): Unit = {
+//    val env = StreamExecutionEnvironment.getExecutionEnvironment
+//    val bsSettings = EnvironmentSettings.newInstance()
+//      .useBlinkPlanner()
+//      .inStreamingMode()
+//      .build()
+////    val bsTableEnv = StreamTableEnvironment.create(env, bsSettings)
+//    val bsTableEnv = TableEnvironment.create(bsSettings)
+//
+//    val properties = new Properties()
+//    properties.setProperty("bootstrap.servers", "node01:9092,node02:9092,node03:9092")
+//    properties.setProperty("zookeeper.connect", "node01:2181,node02:2181,node03:2181")
+//    properties.setProperty("group.id", "test")
+//
+//    val ds: DataStream[(String, Int)] = env.addSource(
+//      new FlinkKafkaConsumer[String]("test",
+//        new SimpleStringSchema(),
+//        properties)
+//    ).map(line => {
+//      val strs = line.split(",")
+//      val name = strs(0).toString
+//      val age = Integer.valueOf(strs(1)).intValue()
+//      (name,  age)
+//    })
+//
+////    org.apache.flink.table.expressions.Expression
+//
+//    val table = bsTableEnv.createTemporaryView("pp", ds)
+//
+//    val sinkDDL =
+//      """
+//        CREATE TABLE people (
+//        name VARCHAR,
+//        age INT
+//        ) WITH (
+//                 'connector.type' = 'jdbc',
+//                 'connector.url' = 'jdbc:mysql://localhost:3306/big',
+//                 'connector.table' = 'people',
+//                 'connector.username' = 'root',
+//                 'connector.password' = '123',
+//                 'connector.write.flush.max-rows' = '1'
+//        )
+//      """
+//    bsTableEnv.sqlUpdate(sinkDDL);
+//
+//    bsTableEnv.sqlUpdate("insert into people " +
+//      "select name,age from pp");
+//
+//
+//    env.execute("TableDemo")
+//  }
+//}
+//
