573657dbbcc67ac03fefe27bfa848876e9a3f10b
NingQue001 <296480474@qq.com>
2021-01-02 12:20:26 +0800

    DeltaTrigger应用之区间测速

---
+++ src/main/java/com/anven/windowDemo/FlinkDeltaTriggerDemo.java
@@ -0,0 +1,132 @@
+package com.anven.windowDemo;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.delta.DeltaFunction;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.streaming.api.windowing.evictors.Evictor;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.DeltaTrigger;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.runtime.operators.windowing.TimestampedValue;
+import org.apache.flink.table.shaded.org.joda.time.DateTime;
+import org.apache.flink.table.shaded.org.joda.time.DateTimeZone;
+import org.apache.flink.util.Collector;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Iterator;
+
+/**
+ *
+ *
+ */
+public class FlinkDeltaTriggerDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        DataStream<Tuple4<Integer, Integer, Double, Long>> carData = env.fromCollection(Arrays.asList(
+                new Tuple4(1, 90, 0d, getTime("2020-12-30 20:01:00")),
+                new Tuple4(1, 85, 3000d, getTime("2020-12-30 20:02:00")),
+                new Tuple4(1, 200, 6000d, getTime("2020-12-30 20:03:00")),
+                new Tuple4(1, 100, 10002d, getTime("2020-12-30 20:04:00")),
+
+                new Tuple4(1, 90, 10006d, getTime("2020-12-30 20:05:00")),
+                new Tuple4(1, 299, 15000d, getTime("2020-12-30 20:07:00")),
+                new Tuple4(1, 175, 20019d, getTime("2020-12-30 20:06:00")),
+
+                new Tuple4(2, 90, 0d, getTime("2020-12-30 20:08:00")),
+                new Tuple4(2, 85, 3000d, getTime("2020-12-30 20:10:00")),
+                new Tuple4(2, 185, 10005d, getTime("2020-12-30 20:09:00")),
+
+                new Tuple4(2, 150, 10006d, getTime("2020-12-30 20:12:00")),
+                new Tuple4(2, 184, 21000d, getTime("2020-12-30 20:11:00"))
+        ));
+
+//        DataStream topSpeeds =
+                carData
+                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Tuple4<Integer, Integer, Double, Long>>(Time.minutes(1)) {
+                    @Override
+                    public long extractTimestamp(Tuple4<Integer, Integer, Double, Long> element) {
+                        return element.f3;
+                    }
+                })
+                .keyBy(0)
+                .window(GlobalWindows.create())
+                .trigger(DeltaTrigger.of(10000,
+                        new DeltaFunction<Tuple4<Integer, Integer, Double, Long>>() {
+                            @Override
+                            public double getDelta(Tuple4<Integer, Integer, Double, Long> oldDataPoint, Tuple4<Integer, Integer, Double, Long> newDataPoint) {
+                                return newDataPoint.f2 - oldDataPoint.f2;
+                            }
+                        }, carData.getType().createSerializer(env.getConfig())))
+                .evictor(new Evictor<Tuple4<Integer, Integer, Double, Long>, GlobalWindow>() {
+                    @Override
+                    public void evictBefore(Iterable<TimestampedValue<Tuple4<Integer, Integer, Double, Long>>> elements, int size, GlobalWindow window, EvictorContext evictorContext) {
+
+                    }
+
+                    @Override
+                    public void evictAfter(Iterable<TimestampedValue<Tuple4<Integer, Integer, Double, Long>>> elements, int size, GlobalWindow window, EvictorContext evictorContext) {
+                        Iterator<TimestampedValue<Tuple4<Integer, Integer, Double, Long>>> iterator = elements.iterator();
+                        while (iterator.hasNext()) {
+                            iterator.next();
+                            iterator.remove();
+                        }
+
+                    }
+                })
+                .aggregate(new MyAggregateFunction(), new ProcessWindowFunction<Integer, String, Tuple, GlobalWindow>() {
+                    @Override
+                    public void process(Tuple tuple, Context context, Iterable<Integer> elements, Collector<String> out) throws Exception {
+                        out.collect("Key" + tuple.toString() + " TopSpeed: " + elements.iterator().next());
+                    }
+                })
+                .print();
+
+
+//        topSpeeds.print();
+
+        env.execute();
+
+    }
+
+    public static long getTime(String strDate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+        Date date = sdf.parse(strDate);
+        return date.getTime();
+    }
+
+    private static class MyAggregateFunction implements AggregateFunction<Tuple4<Integer, Integer, Double, Long>, Integer, Integer> {
+
+        @Override
+        public Integer createAccumulator() {
+            return 0;
+        }
+
+        @Override
+        public Integer add(Tuple4<Integer, Integer, Double, Long> value, Integer accumulator) {
+            return value.f1 > accumulator ? value.f1 : accumulator;
+        }
+
+        @Override
+        public Integer getResult(Integer accumulator) {
+            return accumulator;
+        }
+
+        @Override
+        public Integer merge(Integer a, Integer b) {
+            return a > b ? a : b;
+        }
+    }
+}
