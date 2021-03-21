f78de70c0ea3ed486e81fac4e77a699f87571814
NingQue001 <296480474@qq.com>
2021-03-21 16:37:42 +0800

    feat：table api

---
+++ src/main/scala/com/anven/tablePro/SensorReading.scala
@@ -0,0 +1,6 @@
+package com.anven.tablePro
+
+/**
+ * 样例类,类似于Java的Java Bean
+ */
+case class SensorReading(id: String, temperature: Double, ts: Long)
---
+++ src/main/scala/com/anven/tablePro/TableExample.scala
@@ -0,0 +1,34 @@
+package com.anven.tablePro
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api._
+import org.apache.flink.table.api.bridge.scala._
+
+object TableExample extends App {
+  val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+  env.setParallelism(1)
+
+  val inputStream: DataStream[String] = env.readTextFile("file:///usr/local/data/flink/sensor.txt")
+
+  // 得到样例类SensorReading的一个DataStream
+  val dataStream: DataStream[SensorReading] = inputStream.map(data => {
+    val dataArray = data.split(",")
+    SensorReading(dataArray(0), dataArray(1).toDouble, dataArray(2).toLong)
+  })
+
+  // 表执行环境
+  val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
+  
+  // DataStream转化为Table
+  val dataTable: Table = tableEnv.fromDataStream(dataStream)
+
+  // 调用table api
+  val resultTable: Table = dataTable.select("id, temperature")
+    .filter("id == '1' ")
+
+  // 使用隐式转换为DataStream
+  val resultStream: DataStream[(String, Double)] = resultTable.toAppendStream[(String, Double)]
+  resultStream.print()
+
+  env.execute("TableExample")
+}
