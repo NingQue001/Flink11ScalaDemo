5ead863bbcc5a42c85b0ed3bc65f7eb7e35bca46
NingQue001 <296480474@qq.com>
2021-04-07 23:27:25 +0800

    feat：retract stream（回撤流）

--- src/main/scala/com/anven/tableSqlPro/SqlPro.scala
+++ src/main/scala/com/anven/tableSqlPro/SqlPro.scala
@@ -5,22 +5,28 @@ import org.apache.flink.table.api._
 import org.apache.flink.table.api.bridge.scala._
 import org.apache.flink.table.descriptors._
 
+/**
+ *  1 Table & SQL
+ *  2 回撤流：false 回撤 true insert
+ */
 object SqlPro {
   def main(args: Array[String]): Unit = {
+    val filePath = "file:///usr/local/data/flink/sensor.txt"
+
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
 
     val tableEnv = StreamTableEnvironment.create(env)
 
     // 1 连接外部数据源
-    tableEnv.connect(new FileSystem().path("file:///usr/local/data/flink/sensor.txt"))
+    tableEnv.connect(new FileSystem().path(filePath))
       .withFormat(new Csv) // 设置文件格式处理类
-      .withSchema(new Schema()
+      .withSchema(new Schema() // 设置schema
         .field("id", DataTypes.STRING())
         .field("temperature", DataTypes.DOUBLE())
         .field("ts", DataTypes.BIGINT())
       )
-      .createTemporaryTable("inputTable")
+      .createTemporaryTable("inputTable") // 创建临时表
 
     // 2 表的查询转换
     val sensorTable: Table = tableEnv.from("inputTable")
@@ -31,12 +37,13 @@ object SqlPro {
     // 2.2 聚合
     val aggTable = sensorTable
       .groupBy('id)
-      .select('id, 'id.count() as 'count)
+      .select('id, 'id.count() as 'count) // 调用count()函数
 
     // 3 测试输出
     // Table隐式转化为Stream需引入 import org.apache.flink.table.api.bridge.scala._
     resultTable.toAppendStream[(String, Double, Long)].print("inputTable")
     // 3.1 回撤流(如果key存在，则产生一个delete和一个insert数据流）
+    // false：retract message  true：add message
     aggTable.toRetractStream[(String, Long)].print("aggTable")
 
     env.execute("SqlPro")
